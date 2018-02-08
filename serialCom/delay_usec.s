4e9b7aaa8dbcd1e0e2e5e66a6619e5f480322231
unknown <cookab1@CS-317-9K6NFQ1.appd.appstate.edu>
2018-02-08 15:53:18 -0500

    fixing delay_usec

--- serialCom/delay_usec.s
+++ serialCom/delay_usec.s
@@ -7,24 +7,27 @@
  */ 
 
 		.text
-		.global add2
 		.global delay_usec
-		.global delay
-add2:
-		add r24, r22
-		adc r25, r23
-		ret
 
 delay_usec:
-		nop
-		nop
-		dec r24
-		brge L2
-L1:		dec r24
-		call L2
-		nop
-		nop
-		nop
-		nop
-		brge	L1	
-L2:  	ret
+		dec r24		// 1 cycle
+		brlt L3		// 0 ? 1 : 2 maybe L1
+		//nop			// 1
+		//nop			// 1
+		rcall L2	// 4 cycles
+		//brge L1		// 0 ? 1 : 2 useless
+L1:		breq L2		// 0 ? 1 : 2
+		dec r24		// 1 cycle
+		rcall L2	// 4 cycles
+		nop			// 1
+		nop			// 1
+		nop			// 1
+		nop			// 1
+		brge	L1	// 0 ? 1 : 2 
+L2:  	ret			// 5 cycles
+L3:		mov	r24, $65536 // 1
+		brge L1:
+
+// for 0 -- 
+// for 1 -- 1111/1111/1111/111
+// for 2 -- 
